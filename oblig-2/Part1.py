b861fa5a8d8fbb85e88df3db3b4fd0a9b43817a9
Tobias Hallingstad <tobias@hallingstad.me>
2021-10-12 20:01:54 +0200

    Started to work on the report for oblig 2

--- oblig-2/Part1.py
+++ oblig-2/Part1.py
@@ -6,24 +6,42 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_digits
 
-digits = load_digits()
+def run(randomState = 1, maxIter = 300, nLayers = (100,), plot = False):
+    # digits = load_digits()
 
-# X = Data
-# Y = Classification
-X, y = load_digits(return_X_y=True)
+    # X = Data
+    # Y = Classification
+    X, y = load_digits(return_X_y=True)
 
-trainX, testX, trainY, testY = train_test_split(X, y, test_size=0.30)
+    trainX, testX, trainY, testY = train_test_split(X, y, test_size=0.30)
 
-randomState = 1
-maxIter = 300
-nLayers = (200,100)
-clf = MLPClassifier(random_state=randomState, max_iter=maxIter, hidden_layer_sizes=nLayers).fit(trainX, trainY)
+    clf = MLPClassifier(random_state=randomState, max_iter=maxIter, hidden_layer_sizes=nLayers).fit(trainX, trainY)
 
-print(clf.score(testX, testY))
+    # predict = clf.predict_proba(testX[:8])
+    # plt.matshow(predict)
+    # plt.show()
 
-# predict = clf.predict_proba(testX[:8])
-# plt.matshow(predict)
-# plt.show()
+    return clf.score(testX, testY)
+
+if __name__ == '__main__':
+    for i in range(5):
+        score = run(randomState=1,
+                    maxIter=10,
+                    nLayers=(50,))
+
+        print(f'Run {i+1}| Score: {round(score * 100, 2)}%')
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ oblig-2/task.md
@@ -0,0 +1,14 @@
+This assignment will cover:
+
+Understand usage of `MLPClassifier`
+
+In this assignment, you will implement an Artificial Neural Network with the help of Sklearn library provided class “MLPClassifier” to classify numeric digits (0-9)
+
+- Import all the required libraries (for example, pandas, numpy, matplotlib, etc)
+- Use datasets.load_digits() function from Sklearn library
+- See : https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_digits.html
+- Split 70% of the data to the training set while 30% of the data to test set
+- Use the methods/functions in MLPClassifier class to classify MNIST numeral dataset
+- Try with different number of hidden layers and number of nodes in each of those hidden layers
+- Flip the train and test set and observe the difference in performance
+- Write a short report based on your observations with respect to number of nodes and number of hidden layers and your obtained accuracy on test set
